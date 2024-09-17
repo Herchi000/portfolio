6d1292e9157e9f16396fb4c2f6d0fab702bb4254
Hugo Fernandez <hugofer000@gmail.com>
2024-09-17 16:28:24 -0300

    add project base structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from "@angular/common";
 import { Component, inject, OnInit } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { ThemeService } from "./services/theme.service";
+import { ProjectComponent } from "./components/project/project.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, ProjectComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
